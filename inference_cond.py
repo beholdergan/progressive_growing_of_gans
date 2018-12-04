54aa92a4874b23da92f0e55bfa7bde7d3547428e
deanzadok <danielxp75@gmail.com>
2018-12-04 19:42:10 +0200

    changed inference script to generate multiple examples

--- inference_cond.py
+++ inference_cond.py
@@ -8,8 +8,8 @@ import tfutil
 # manual parameters
 run_id = "022-pgan-beautydataset-cond-preset-v2-1gpu-fp32"
 result_subdir = misc.create_result_subdir('results', 'inference_test')
+num_of_examples = 500
 num_of_labels = 10
-random_seed = 1500 # change it to control the generated latent vector
 
 misc.init_output_logging()
 
@@ -23,29 +23,38 @@ tf_config = EasyDict() # TensorFlow session config, set by tfutil.init_tf().
 tf_config['graph_options.place_pruned_graph'] = True # False (default) = Check that all ops are available on the designated device.
 tfutil.init_tf(tf_config)
 
-# initialize random seed
-np.random.seed(random_seed)
-random_state = np.random.RandomState(random_seed)
-
 #load network
 network_pkl = misc.locate_network_pkl(run_id)
 print('Loading network from "%s"...' % network_pkl)
 G, D, Gs = misc.load_network_pkl(run_id, None)
 
-# generate random noise
-latents = misc.random_latents(1, Gs, random_state=random_state)
+# iterate random seed to generate [num_of_examples] examples of GIFs
+for j in range(num_of_examples):
 
-for i in range(num_of_labels):
-    
-    # initiate conditioned label
-    labels = np.zeros([1, num_of_labels], np.float32)
-    labels[0][i] = 1.0
-    
-    # infer conditioned noise to receive image
-    image = Gs.run(latents, labels, minibatch_size=1, num_gpus=1, out_mul=127.5, out_add=127.5, out_shrink=1, out_dtype=np.uint8)
+    random_seed = j # change it to control the generated latent vector
 
-    # save generated image as 'i.png' and noise vector as noise_vector.txt
-    misc.save_image_grid(image, os.path.join(result_subdir, '{}.png'.format(i)), [0,255], [1,1])
+    # initialize random seed
+    np.random.seed(random_seed)
+    random_state = np.random.RandomState(random_seed)
 
-    # save latent space for later use
-    np.save(os.path.join(result_subdir,'latents_vector.npy'), latents)
+    # generate random noise
+    latents = misc.random_latents(1, Gs, random_state=random_state)
+
+    for i in range(num_of_labels):
+        
+        # initiate conditioned label
+        labels = np.zeros([1, num_of_labels], np.float32)
+        labels[0][i] = 1.0
+        
+        # infer conditioned noise to receive image
+        image = Gs.run(latents, labels, minibatch_size=1, num_gpus=1, out_mul=127.5, out_add=127.5, out_shrink=1, out_dtype=np.uint8)
+
+        # save generated image as 'i.png' and noise vector as noise_vector.txt
+        misc.save_image_grid(image, os.path.join(result_subdir, '{}_{}.png'.format('%04d' % j,i)), [0,255], [1,1])
+
+        # save latent space for later use
+        np.save(os.path.join(result_subdir,'latents_vector.npy'), latents)
+
+    if j % 10 == 0:
+        print("saved {}/{} images".format(j,num_of_examples))
+
