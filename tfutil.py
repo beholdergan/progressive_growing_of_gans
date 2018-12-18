4be1c1ab2dbc079773ece7ac9eccc9e4054ebd11
deanzadok <danielxp75@gmail.com>
2018-12-18 19:42:32 +0200

    fixed latest commit

--- tfutil.py
+++ tfutil.py
@@ -21,10 +21,10 @@ from tensorflow.python import debug as tf_debug
 def run(*args, **kwargs): # Run the specified ops in the default session.
     
     # GUI tensorflow debugger using tensorboard
-    session = tf.Session()
-    with tf_debug.TensorBoardDebugWrapperSession(session, 'localhost:6064') as sess:
-        sess.run(*args, **kwargs)
-    return
+    #session = tf.Session()
+    #with tf_debug.TensorBoardDebugWrapperSession(session, 'localhost:6064') as sess:
+    #    sess.run(*args, **kwargs)
+    #return
     
     # CLI tensorflow debugger
     #session = tf.Session()
@@ -36,7 +36,7 @@ def run(*args, **kwargs): # Run the specified ops in the default session.
     #    import pdb
     #    pdb.set_trace()
 
-    #return tf.get_default_session().run(*args, **kwargs)
+    return tf.get_default_session().run(*args, **kwargs)
 
 def is_tf_expression(x):
     return isinstance(x, tf.Tensor) or isinstance(x, tf.Variable) or isinstance(x, tf.Operation)
